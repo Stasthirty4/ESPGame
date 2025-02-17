a901bca08b9451b88d3f80bd1ec20f5a94d9e1d7
Stanislav (Stas) Litvak <40701225+Stasthirty4@users.noreply.github.com>
2025-02-17 16:57:35 -0500

    Spring 2025 Ver.
    changed file to include a recurring menu to select amount of colors used by the program.

--- src/espgame/ESPGame.java
+++ src/espgame/ESPGame.java
@@ -14,98 +14,143 @@ package espgame;
 import java.util.Scanner;
 import java.util.Random;
 import java.io.*;
+
+
 public class ESPGame {
 		
 	public static void main(String[] args) throws IOException {
 		
 		Scanner keyboard = new Scanner(System.in);
-		String userColorChoice, compColorChoice;
+		String userColorChoice, compColorChoice, userNumberColorChoice;
 		int correctGuesses = 0;
+		int userChoice=0;
+		int numColorDisplay = 0;
 		Random rngNumber = new Random();
+		PrintWriter outputFile = new PrintWriter("EspGameResults.txt");
 		System.out.println("CMSC203 Assignment1: Test your ESP skills!");
 		
-		System.out.print("Enter the filename: ");
-		String fileName = keyboard.nextLine();
-		Scanner inputFile = new Scanner(new File(fileName));
-		System.out.println("There are sixteen colors from a file:");
-		
-		while (inputFile.hasNext()) 
+		while(true)
 		{
-			String nextLine = inputFile.nextLine();
-			System.out.println(nextLine);
-		}
+		System.out.println("Would you please choose one of the 4 options from the menu:");
+		System.out.println("1-	Read and display on the screen first 16 names of colors from a file colors.txt, so the player can select one of them names of colors.");
+		System.out.println("2-	Read and display on the screen first 10 names of colors from a file colors.txt, so the player can select one of them names of colors.");
+		System.out.println("3-	Read and display on the screen first 5 names of colors from a file colors.txt, so the player can select one of them names of colors.");
+		System.out.println("4-	Exit from a program\n");
+		System.out.println("Enter your option: ");
 		
-		System.out.println();
-		inputFile.close();
 		
-		for (int count = 1; count <= 3; count++) {
-			
-			int selection = rngNumber.nextInt(16) + 1;
-			switch(selection)
+		userNumberColorChoice = keyboard.nextLine();
+		
+		userChoice=Integer.parseInt(userNumberColorChoice);
+		
+
+		
+		switch(userChoice) {
+			case 1:
+				numColorDisplay=16;
+				break;
+			case 2:
+				numColorDisplay=10;
+				break;
+			case 3:
+				numColorDisplay=5;
+				break;
+			case 4:
+				System.out.println("Game Over");
+				numColorDisplay =0;
+				break;
+			default:
 			{
-			case 1: 
-				compColorChoice = "Black";
-				break;
-			case 2: 
-				compColorChoice = "White";
-				break;
-			case 3: 
-				compColorChoice = "Gray";
-				break;
-			case 4: 
-				compColorChoice = "Silver";
-				break;
-			case 5: 
-				compColorChoice = "Maroon";
-				break;
-			case 6: 
-				compColorChoice = "Red";
-				break;
-			case 7: 
-				compColorChoice = "Purple";
-				break;
-			case 8: 
-				compColorChoice = "Fuschia";
-				break;
-			case 9: 
-				compColorChoice = "Green";
-				break;
-			case 10: 
-				compColorChoice = "Lime";
-				break;
-			case 11: 
-				compColorChoice = "Olive";
-				break;
-			case 12: 
-				compColorChoice = "Yellow";
-				break;
-			case 13: 
-				compColorChoice = "Navy";
-				break;
-			case 14: 
-				compColorChoice = "Blue";
-				break;
-			case 15: 
-				compColorChoice = "Teal";
-				break;
-			case 16: 
-				compColorChoice = "Aqua";
-				break;
-			default: 
-				compColorChoice = "Invalid Random Number";
+				System.out.println("Invalid number. Game over.");
+				System.exit(0);
 			}
-		System.out.println("Round " + count + "\n");
-		System.out.println("I am thinking of a color.");
-		System.out.println("Is it one of list of colors above?");
-		System.out.println("Enter your guess:");
-		userColorChoice = keyboard.nextLine();
-		if (userColorChoice.equalsIgnoreCase(compColorChoice)) 
-			correctGuesses++;
-		System.out.println("\nI was thinking of " + compColorChoice + ".");
 		}
+		if (numColorDisplay!=0){
+			System.out.print("Enter the filename: ");
+			
+			String fileName = keyboard.nextLine();
+			Scanner inputFile = new Scanner(new File(fileName));
+			
+			System.out.println("There are sixteen colors from a file.");
+			for (int i=0; i<numColorDisplay; i++) {
+				String nextLine = inputFile.nextLine();
+				System.out.println(nextLine);
+			}
+			
+			System.out.println();
+			inputFile.close();
+	
+			
+			for (int count = 1; count <= 3; count++) {
+				
+				int selection = rngNumber.nextInt(numColorDisplay) + 1;
+				switch(selection)
+				{
+				case 1: 
+					compColorChoice = "Black";
+					break;
+				case 2: 
+					compColorChoice = "White";
+					break;
+				case 3: 
+					compColorChoice = "Gray";
+					break;
+				case 4: 
+					compColorChoice = "Silver";
+					break;
+				case 5: 
+					compColorChoice = "Maroon";
+					break;
+				case 6: 
+					compColorChoice = "Red";
+					break;
+				case 7: 
+					compColorChoice = "Purple";
+					break;
+				case 8: 
+					compColorChoice = "Fuschia";
+					break;
+				case 9: 
+					compColorChoice = "Green";
+					break;
+				case 10: 
+					compColorChoice = "Lime";
+					break;
+				case 11: 
+					compColorChoice = "Olive";
+					break;
+				case 12: 
+					compColorChoice = "Yellow";
+					break;
+				case 13: 
+					compColorChoice = "Navy";
+					break;
+				case 14: 
+					compColorChoice = "Blue";
+					break;
+				case 15: 
+					compColorChoice = "Teal";
+					break;
+				case 16: 
+					compColorChoice = "Aqua";
+					break;
+				default: 
+					compColorChoice = "Invalid Random Number";
+				}
+			System.out.println("Round " + count + "\n");
+			System.out.println("I am thinking of a color.");
+			System.out.println("Is it one of list of colors above?");
+			System.out.println("Enter your guess:");
+			userColorChoice = keyboard.nextLine();
+			if (userColorChoice.equalsIgnoreCase(compColorChoice)) 
+				correctGuesses++;
+			System.out.println("\nI was thinking of " + compColorChoice + ".");
+			}
+			
+			System.out.println("Game Over");
+			System.out.println("You guessed " + correctGuesses + " out of 3 colors correctly.");
+}
 		String studentName, description, dueDate;
-		System.out.println("Game Over");
-		System.out.println("You guessed " + correctGuesses + " out of 3 colors correctly.");
 		System.out.println("Enter your name: ");
 		studentName = keyboard.nextLine();
 		System.out.println("Describe yourself: ");
@@ -115,5 +160,29 @@ public class ESPGame {
 		System.out.println("User Name: " + studentName);
 		System.out.println("User Description: " + description);
 		System.out.println("Due Date: " + dueDate);
-	}		
+		
+		outputFile.println("Game Over");
+		outputFile.println("You guessed " + correctGuesses + " out of 3 colors correctly.");
+		outputFile.println("User Name: " + studentName);
+		outputFile.println("User Description: " + description);
+		outputFile.println("Due Date: " + dueDate);
+		
+		if (numColorDisplay!=0){
+		 System.out.println("Do you want to continue? Yes/No");
+		 String answer = keyboard.nextLine();
+		 
+		    if (answer.equalsIgnoreCase("Yes")) {
+		        continue;
+		    } else {
+		       System.out.println("Goodbye!");
+		       break;
+		    }
+		}else {
+			break;
+		}
+	
+	}
+		keyboard.close();
+		outputFile.close();
+	}
 }
